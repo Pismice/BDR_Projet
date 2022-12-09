b5dbf35dda6c8b68c46e87c7dd84dbd1dbc5a486
Oscar Baume <94351326+obaume@users.noreply.github.com>
2022-12-09 16:29:02 +0100

    constraints -> constraint

--- script/create.sql
+++ script/create.sql
@@ -15,7 +15,7 @@ DROP TABLE IF EXISTS Pays CASCADE;
 CREATE TABLE Pays(
 	id SMALLSERIAL,
 	nom VARCHAR(25) not null,
-	CONSTRAINTS PK_Pays PRIMARY KEY(id)
+	CONSTRAINT PK_Pays PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS Agent CASCADE;
@@ -23,14 +23,14 @@ CREATE TABLE Agent(
 	id SMALLSERIAL,
 	nom varchar(20) not null,
 	role AGENT_TYPE not null,
-	CONSTRAINTS PK_Agent PRIMARY KEY(id)
+	CONSTRAINT PK_Agent PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS Carte CASCADE;
 CREATE TABLE Carte(
 	id SMALLSERIAL,
 	nom varchar(20) not null,
-	CONSTRAINTS PK_Carte PRIMARY KEY(id)
+	CONSTRAINT PK_Carte PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS Joueur CASCADE;
@@ -42,8 +42,8 @@ CREATE TABLE Joueur(
 	age INTEGER not null,
 	salaire DECIMAL not NULL,
 	idPays INTEGER not null,
-	CONSTRAINTS PK_Joueur PRIMARY KEY(id),
-	CONSTRAINTS FK_Joueur_idPays FOREIGN KEY (idPays) REFERENCES Pays(id)
+	CONSTRAINT PK_Joueur PRIMARY KEY (id),
+	CONSTRAINT FK_Joueur_idPays FOREIGN KEY (idPays) REFERENCES Pays(id)
 );
 
 DROP TABLE IF EXISTS Equipe CASCADE;
@@ -52,15 +52,15 @@ CREATE TABLE Equipe(
 	nom VARCHAR(20),
 	elo INTEGER,
 	region REGION,
-	CONSTRAINTS PK_Equipe PRIMARY KEY(id)
+	CONSTRAINT PK_Equipe PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS Joueur_Equipe CASCADE;
 CREATE TABLE Joueur_Equipe(
 	idJoueur INTEGER NOT NULL,
 	idEquipe INTEGER NOT NULL,
-	CONSTRAINTS FK_Joueur_Equipe_idJoueur FOREIGN KEY(idJoueur) REFERENCES Joueur(id),
-	CONSTRAINTS FK_Joueur_Equipe_idEquipe FOREIGN KEY(idEquipe) REFERENCES Equipe(id)
+	CONSTRAINT FK_Joueur_Equipe_idJoueur FOREIGN KEY (idJoueur) REFERENCES Joueur(id),
+	CONSTRAINT FK_Joueur_Equipe_idEquipe FOREIGN KEY (idEquipe) REFERENCES Equipe(id)
 );
 
 DROP TABLE IF EXISTS Tournoi CASCADE;
@@ -71,15 +71,15 @@ CREATE TABLE Tournoi(
 	point INTEGER,
 	dateDebut DATE,
 	dateFin DATE,
-	CONSTRAINTS PK_Tournoi PRIMARY KEY(id)
+	CONSTRAINT PK_Tournoi PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS Tournoi_Equipe CASCADE;
 CREATE TABLE Tournoi_Equipe(
 	idTournoi INTEGER NOT NULL,
 	idEquipe INTEGER NOT NULL,
-	CONSTRAINTS FK_Tournoi_Equipe_idTournoi FOREIGN KEY(idTournoi) REFERENCES Tournoi(id),
-	CONSTRAINTS FK_Tournoi_Equipe_idEquipe FOREIGN KEY(idEquipe) REFERENCES Equipe(id)
+	CONSTRAINT FK_Tournoi_Equipe_idTournoi FOREIGN KEY (idTournoi) REFERENCES Tournoi(id),
+	CONSTRAINT FK_Tournoi_Equipe_idEquipe FOREIGN KEY (idEquipe) REFERENCES Equipe(id)
 );
 
 DROP TABLE IF EXISTS Match CASCADE;
@@ -87,5 +87,5 @@ CREATE TABLE Match(
 	idTournoi INTEGER NOT NULL,
 	game_format GAME_FORMAT,
 	game_date DATE,
-	CONSTRAINTS FK_Match_idTournoi FOREIGN KEY(idTournoi) REFERENCES Tournoi(id)
+	CONSTRAINT FK_Match_idTournoi FOREIGN KEY (idTournoi) REFERENCES Tournoi(id)
 );
